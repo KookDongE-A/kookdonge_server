e856e2c991c09af7ad247c3472df1d16c6951a7e
snacktime <tth8181@gmail.com>
2025-09-09 11:02:18 +0900

    [KDE-9] feat: spring 프로젝트 시작

---
+++ src/main/java/com/kookdonge/kookdonge_server/KookdongeServerApplication.java
@@ -0,0 +1,13 @@
+package com.kookdonge.kookdonge_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KookdongeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KookdongeServerApplication.class, args);
+	}
+
+}
