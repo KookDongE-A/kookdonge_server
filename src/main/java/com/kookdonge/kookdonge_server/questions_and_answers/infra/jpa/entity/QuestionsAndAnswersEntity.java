edf64d498c3b32f92b75749c081f6e05deb8ca30
wngktjd13 <seonguk3553@kookmin.ac.kr>
2025-09-25 02:54:20 +0900

    [KDE-30] feat: 질문 목록 조회 API 구현

---
+++ src/main/java/com/kookdonge/kookdonge_server/questions_and_answers/infra/jpa/entity/QuestionsAndAnswersEntity.java
@@ -0,0 +1,42 @@
+package com.kookdonge.kookdonge_server.questions_and_answers.infra.jpa.entity;
+
+import com.kookdonge.kookdonge_server.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Entity
+@Table(name = "questions_and_answers")
+public class QuestionsAndAnswersEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "questions_and_answers_id")
+    private Long questionsAndAnswersId;
+
+    @NotNull
+    @Column(name = "club_id", nullable = false)
+    private Long clubId;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String question;
+
+    @Column
+    private String answer;
+
+    public static QuestionsAndAnswersEntity of(Long clubId, String question) {
+        return new QuestionsAndAnswersEntity(null, clubId, question, null);
+    }
+
+    public void updateAnswer(String answer) {
+        this.answer = answer;
+    }
+}
---
+++ src/main/java/com/kookdonge/kookdonge_server/questions_and_answers/presentation/dto/req/QuestionCreateReq.java
@@ -0,0 +1,15 @@
+package com.kookdonge.kookdonge_server.questions_and_answers.presentation.dto.req;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuestionCreateReq {
+
+    @NotBlank(message = "질문은 필수입니다")
+    private String question;
+}
